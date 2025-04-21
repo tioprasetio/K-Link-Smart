c98c15dac22bd9baddb4779a75b80a454922dc36
Tio Yudho Prasetio <tioyudhoprasetio33@gmail.com>
2025-04-21 23:47:26 +0700

    fix: reintroduce BrowserRouter in main.tsx for routing support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import "flowbite";
 import DarkModeContextProvider from "./context/DarkMode.tsx";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
