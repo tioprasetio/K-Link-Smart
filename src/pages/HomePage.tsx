a80fb2b5b7e5beb1add1798698c5ab80a343964b
Tio Yudho Prasetio <tioyudhoprasetio33@gmail.com>
2025-06-10 13:17:53 +0700

    refactor: reduce gap size in grid layout on HomePage for improved visual consistency

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -197,7 +197,7 @@ const HomePage = () => {
                 </span>
               </button>
             </div>
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
               {loading
                 ? Array.from({ length: 4 }).map((_, index) => (
                     <SkeletonListProduct key={index} />
